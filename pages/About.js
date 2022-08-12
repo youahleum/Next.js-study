1ea056902debb5cdfc446356fcb95a0bc1341625
youahleum <dkfma6033@naver.com>
2022-08-12 23:14:00 +0900

    head feat

--- pages/About.js
+++ pages/About.js
@@ -1,6 +1,11 @@
+import Head from "next/head";
+
 export default function about() {
   return (
     <>
+      <Head>
+        <title>About | Next Movies</title>
+      </Head>
       <h1>about</h1>
     </>
   );
--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,9 @@ import Head from "next/head";
 export default function Home() {
   return (
     <div>
+      <Head>
+        <title>Home | Next Movies</title>
+      </Head>
       <h1>Hello</h1>
       <style jsx global>
         {`
