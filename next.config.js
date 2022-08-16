0abaf0cfe0c3ccce943ef42f3eeb1a44879b85dc
youahleum <dkfma6033@naver.com>
2022-08-16 17:08:25 +0900

    api key 설정

--- next.config.js
+++ next.config.js
@@ -1,6 +1,26 @@
 /** @type {import('next').NextConfig} */
+
+const API_KEY = process.env.API_KEY;
+
 const nextConfig = {
   reactStrictMode: true,
-}
+  async redirects() {
+    return [
+      {
+        source: "/old-blog/:path*", //sorce찾아 destination으로 redirects 시켜준다
+        destination: "/new-blog/:path*",
+        permanent: false, // 검색엔진이 이 정보를 기억하는지에 대한 여부
+      },
+    ];
+  },
+  async rewrites() {
+    return [
+      {
+        source: "/api/movies",
+        destination: `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`,
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
