9dad87b803ca71df4056e721984c976b4723f257
youahleum <dkfma6033@naver.com>
2022-08-16 19:11:05 +0900

    getServerSideProps 적용

--- pages/index.js
+++ pages/index.js
@@ -2,21 +2,21 @@ import Head from "next/head";
 import { useEffect, useState } from "react";
 import Seo from "../components/Seo";
 
-export default function Home() {
-  const [movies, setMovies] = useState([]);
-  useEffect(() => {
-    (async () => {
-      const { results } = await (await fetch(`/api/movies`)).json();
-      setMovies(results);
-      console.log(results);
-    })();
-  }, []);
+export default function Home({ results }) {
+  // const [movies, setMovies] = useState([]);
+  // useEffect(() => {
+  //   (async () => {
+  //     // const { results } = await (await fetch(`/api/movies`)).json();
+  //     setMovies(results);
+  //     console.log(results);
+  //   })();
+  // }, []);
 
   return (
     <div className="container">
       <Seo title="Home" />
-      {!movies && <h4>Loading...</h4>}
-      {movies?.map((movie) => (
+      {/* {!movies && <h4>Loading...</h4>} */}
+      {results?.map((movie) => (
         <div className="movie" key={movie.id}>
           <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} />
           <h4>{movie.original_title}</h4>
@@ -45,4 +45,15 @@ export default function Home() {
       `}</style>
     </div>
   );
+}
+
+// 이 안에서 어떤 코드를 쓰던 server에서 돌아가게 된다.
+//  client가 아닌 server쪽에서만 작동하는것 -> 이걸 이용해 api키를 숨길수 있기도 하다
+export async function getServerSideProps() {
+  const { results } = await (await fetch(`http://localhost:3000/api/movies`)).json();
+  return {
+    props: {
+      results,
+    },
+  };
 }
