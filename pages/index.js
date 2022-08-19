b7a1631d7589cb11f8af039fc47cd102d221179a
youahleum <dkfma6033@naver.com>
2022-08-20 05:32:07 +0900

    movie detail page

--- pages/index.js
+++ pages/index.js
@@ -5,18 +5,49 @@ import { useEffect, useState } from "react";
 import Seo from "../components/Seo";
 
 export default function Home({ results }) {
+  console.log(results);
   const router = useRouter();
-  const onClick = (id, title) => {
-    router.push(`/movies/${title}/${id}`);
+  // const onClick = (id, title) => {
+  //   router.push(`/movies/${title}/${id}`);
+  // };
+
+  // const onClick = (id, title, overview, poster_path) => {
+  //   // console.log({ movie });
+  //   router.push({
+  //     pathname: `/movies/${title}/${id}`,
+  //     query: {
+  //       id,
+  //       title,
+  //       overview,
+  //       poster_path,
+  //     },
+  //   });
+  // };
+
+  const onClick = (movie) => {
+    // console.log({ movie });
+    router.push(
+      {
+        pathname: `/movies/${movie.title}/${movie.id}`,
+        query: {
+          id: movie.id,
+          title: movie.title,
+          overview: movie.overview,
+          poster_path: movie.poster_path,
+        },
+      },
+      `/movies/${movie.title}/${movie.id}`
+    );
   };
+
   return (
     <div className="container">
       <Seo title="Home" />
       {results?.map((movie) => (
-        <div className="movie" key={movie.id} onClick={() => onClick(movie.id, movie.original_title)}>
+        // <div className="movie" key={movie.id} onClick={() => onClick(movie.id, movie.original_title, movie.overview, movie.poster_path)}>
+        <div className="movie" key={movie.id} onClick={() => onClick(movie)}>
           <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} />
           <h4>
-            {" "}
             <Link href={`/movies/${movie.original_title}/${movie.id}`}>
               <a>{movie.original_title}</a>
             </Link>
--- pages/movies/[...params].js
+++ pages/movies/[...params].js
@@ -1,38 +1,64 @@
 import { useRouter } from "next/router";
 import Seo from "../../components/Seo";
 
-export default function Detail({ params }) {
+export default function Detail() {
+  // 3. csr로 받은 props
+  // export default function Detail({ params }) {
   const router = useRouter();
 
   //1.  아래처럼만 입력을 해주게 된다면 url로 넘어갔을때 server error가 뜨게 된다
   // const [title, id] = router.query.params;
+
   //2. 그래서 아래처럼 뒤에  || [] 이렇게 추가해주면 에러가 뜨는것을 막을수 있다
   // 아래는 csr로 렌더링 해준면서 에러를 없애준것
-  //   const [title, id] = router.query.params || [];
-  //   console.log(router);
-  //   return (
-  //     <div>
-  //       {/* <Seo title={title} /> */}
-  //       <h4>{title || "Loading..."}</h4>
-  //     </div>
-  //   );
-  // }
-
-  //3. 위는 csr 방식으로 렌더링해주었다면 아래는 ssr로 렌더링 진행
-  const [title, id] = params || [];
-  console.log(router);
+  const [title, id] = router.query.params || [];
+  console.log("router.query.params", router.query.params);
+  console.log("router.query", router.query);
   return (
     <div>
       <Seo title={title} />
-      <h4>{title}</h4>
+      <img src={`https://image.tmdb.org/t/p/w500${router.query.poster_path}`} />
+      <h3>{title || "Loading..."}</h3>
+      <div className="overveiw">{router.query.overview}</div>
+      <style jsx>{`
+        img {
+          position:relative;
+          left:50%;
+          
+          width: 300px;
+          margin: 30px 0 0 -150px;
+          border-radius: 12px;
+          box-shadow: rgba(0, 0, 0, 0.1) 0px 4px 12px; */
+        }
+
+        h3 {
+          font-size: 24px;
+          text-align: center;
+        }
+        div.overveiw{
+          font-size:16px
+        }
+      `}</style>
     </div>
   );
 }
 
-export function getServerSideProps({ params: { params } }) {
-  return {
-    props: {
-      params,
-    },
-  };
-}
+//3. 위는 csr 방식으로 렌더링해주었다면 아래는 ssr로 렌더링 진행
+//   const [title, id] = params || [];
+//   console.log(router);
+//   console.log(params);
+//   return (
+//     <div>
+//       <Seo title={title} />
+//       <h4>{title}</h4>
+//     </div>
+//   );
+// }
+
+// export function getServerSideProps({ params: { params } }) {
+//   return {
+//     props: {
+//       params,
+//     },
+//   };
+// }
